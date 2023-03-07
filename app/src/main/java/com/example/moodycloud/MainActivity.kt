fd2de757d000353a9cec11e1b20edb38de4da752
Liriani <42298016+liriani@users.noreply.github.com>
2023-03-07 17:24:58 +0100

    Edit MainActivity and more

--- app/src/main/java/com/example/moodycloud/MainActivity.kt
+++ app/src/main/java/com/example/moodycloud/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.moodycloud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnsignInHome= findViewById<Button>(R.id.btn_signInHome)
+        val btnsignUpHome= findViewById<Button>(R.id.btn_signUpHome)
+
+
+        btnsignUpHome.setOnClickListener{
+            val i = Intent(this, HomeActivity::class.java)
+            startActivity(i)
+        }
+
+        btnsignInHome.setOnClickListener{
+            val i = Intent(this,HomeActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
