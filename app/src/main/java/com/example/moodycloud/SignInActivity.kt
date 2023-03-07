cb0c7a830bee372853de774bdc45f19d29aeb92c
Liriani <42298016+liriani@users.noreply.github.com>
2023-03-07 20:19:49 +0100

    Correction name SignIn

--- app/src/main/java/com/example/moodycloud/SignInActivity.kt
+++ app/src/main/java/com/example/moodycloud/SignInActivity.kt
@@ -3,9 +3,9 @@ package com.example.moodycloud
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignUpActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_in)
     }
 }
